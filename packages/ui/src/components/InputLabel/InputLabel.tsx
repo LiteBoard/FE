cfdb44417568259a41a6c440f1358bd2f252c2aa
RohSungKyun <songkyun1108@naver.com>
2025-09-07 17:36:11 +0900

    fix: setting type version

--- packages/ui/src/components/InputLabel/InputLabel.tsx
+++ packages/ui/src/components/InputLabel/InputLabel.tsx
@@ -4,36 +4,40 @@ import React from 'react';
 interface InputLabelProps extends React.InputHTMLAttributes<HTMLInputElement> {
   label?: string;
   required?: boolean;
+  ref?: React.Ref<HTMLInputElement>;
 }
 
-export const InputLabel = React.forwardRef<HTMLInputElement, InputLabelProps>(
-  ({ className, type = 'text', label, required, ...props }, ref) => {
-    return (
-      <div className="flex flex-col space-y-2">
-        {label && (
-          <div className="relative inline-block w-fit">
-            <label className="text-sm font-medium text-neutral-600">
-              {label}
-            </label>
-            {required && (
-              <span className="absolute -top-1 -right-2 text-red-500 text-base leading-none">
-                •
-              </span>
-            )}
-          </div>
-        )}
-        <input
-          type={type}
-          className={cn(
-            'w-[340px] rounded-md border-[1.5px] border-blue-200 px-4 py-3 text-neutral-700 placeholder-neutral-400 focus:outline-none focus:border-blue-500',
-            className
+export const InputLabel = ({ 
+  className, 
+  type = 'text', 
+  label, 
+  required, 
+  ref,
+  ...props 
+}: InputLabelProps) => {
+  return (
+    <div className="flex flex-col space-y-2">
+      {label && (
+        <div className="relative inline-block w-fit">
+          <label className="text-sm font-medium text-neutral-600">
+            {label}
+          </label>
+          {required && (
+            <span className="absolute -top-1 -right-2 text-red-500 text-base leading-none">
+              •
+            </span>
           )}
-          ref={ref}
-          {...props}
-        />
-      </div>
-    );
-  }
-);
-
-InputLabel.displayName = 'InputLabel';
+        </div>
+      )}
+      <input
+        type={type}
+        className={cn(
+          'w-[340px] rounded-md border-[1.5px] border-blue-200 px-4 py-3 text-neutral-700 placeholder-neutral-400 focus:outline-none focus:border-blue-500',
+          className
+        )}
+        ref={ref}
+        {...props}
+      />
+    </div>
+  );
+};
