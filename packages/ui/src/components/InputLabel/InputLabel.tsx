a73fb5dc9a72c03edf9043f2cb5b9c8c5fac0f86
RohSungKyun <songkyun1108@naver.com>
2025-09-14 13:02:27 +0900

    feat: add GitHub Actions workflow

--- packages/ui/src/components/InputLabel/InputLabel.tsx
+++ packages/ui/src/components/InputLabel/InputLabel.tsx
@@ -15,29 +15,29 @@ export const InputLabel = ({
   ref,
   ...props 
 }: InputLabelProps) => {
-  return (
-    <div className="flex flex-col space-y-2">
-      {label && (
-        <div className="relative inline-block w-fit">
-          <label className="text-sm font-medium text-neutral-600">
-            {label}
-          </label>
-          {required && (
-            <span className="absolute -top-1 -right-2 text-red-500 text-base leading-none">
-              •
-            </span>
-          )}
-        </div>
-      )}
-      <input
-        type={type}
-        className={cn(
-          'w-[340px] rounded-md border-[1.5px] border-blue-200 px-4 py-3 text-neutral-700 placeholder-neutral-400 focus:outline-none focus:border-blue-500',
-          className
+    return (
+      <div className="flex flex-col space-y-2">
+        {label && (
+          <div className="relative inline-block w-fit">
+            <label className="text-sm font-medium text-neutral-600">
+              {label}
+            </label>
+            {required && (
+              <span className="absolute -top-1 -right-2 text-red-500 text-base leading-none">
+                •
+              </span>
+            )}
+          </div>
         )}
-        ref={ref}
-        {...props}
-      />
-    </div>
-  );
+        <input
+          type={type}
+          className={cn(
+            'w-[340px] rounded-md border-[1.5px] border-blue-200 px-4 py-3 text-neutral-700 placeholder-neutral-400 focus:outline-none focus:border-blue-500',
+            className
+          )}
+          ref={ref}
+          {...props}
+        />
+      </div>
+    );
 };
